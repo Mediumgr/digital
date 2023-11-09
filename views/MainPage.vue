b0eb1a595672b2c16a534a3aa55e0590ac7177be
Slava <bitrix@maxima.ru>
2023-11-09 18:19:10 +0300

    MainPage

--- views/MainPage.vue
+++ views/MainPage.vue
@@ -1,23 +1,29 @@
 <template>
-<!--   <MainBlock /> -->
-  <SpecialistsInfo />
-<!--   <WorkingSection />
-  <FormVacancy /> -->
+    <MainBlock/>
+    <SpecialistsInfo/>
+    <WorkingSection/>
+    <FormVacancy/>
+    <ProcessBlock/>
+    <LifeOutsideOffice/>
 </template>
 
 <script>
-/* import MainBlock from '@/components/MainBlock.vue'; */
+import MainBlock from '@/components/MainBlock.vue';
 import SpecialistsInfo from '@/components/SpecialistsInfo.vue';
-/* import WorkingSection from '@/components/WorkingSection.vue';
-import FormVacancy from '@/components/FormVacancy.vue'; */
+import WorkingSection from '@/components/WorkingSection.vue';
+import FormVacancy from '@/components/FormVacancy.vue';
+import ProcessBlock from '@/components/ProcessBlock.vue';
+import LifeOutsideOffice from '@/components/LifeOutsideOffice.vue';
 
 export default {
-  name: 'MainPage',
-  components: {
- /*    MainBlock, */
-    SpecialistsInfo,
-/*     WorkingSection,
-    FormVacancy, */
-  },
+    name: 'MainPage',
+    components: {
+        MainBlock,
+        SpecialistsInfo,
+        WorkingSection,
+        FormVacancy,
+        ProcessBlock,
+        LifeOutsideOffice
+    },
 };
 </script>
