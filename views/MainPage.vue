272e7d038b69f00494d22378317ce29a99b55c62
Руслан <Mediumgr@yandex.ru>
2023-11-09 20:18:00 +0300

    processBlock moved to downside

--- views/MainPage.vue
+++ views/MainPage.vue
@@ -3,26 +3,26 @@
   <SpecialistsInfo />
   <WorkingSection />
   <WhichStackToUseSection />
-  <FormVacancy />
   <ProcessBlock />
+  <FormVacancy />
   <MapSection />
   <LifeOutsideOffice />
   <EducationSection />
 </template>
 
 <script>
-import MainBlock from '@/components/MainBlock.vue';
-import SpecialistsInfo from '@/components/SpecialistsInfo.vue';
-import WorkingSection from '@/components/WorkingSection.vue';
-import FormVacancy from '@/components/FormVacancy.vue';
-import ProcessBlock from '@/components/ProcessBlock.vue';
-import LifeOutsideOffice from '@/components/LifeOutsideOffice.vue';
+import MainBlock from "@/components/MainBlock.vue";
+import SpecialistsInfo from "@/components/SpecialistsInfo.vue";
+import WorkingSection from "@/components/WorkingSection.vue";
+import FormVacancy from "@/components/FormVacancy.vue";
+import ProcessBlock from "@/components/ProcessBlock.vue";
+import LifeOutsideOffice from "@/components/LifeOutsideOffice.vue";
 import EducationSection from "@/components/EducationSection.vue";
 import MapSection from "@/components/MapSection.vue";
 import WhichStackToUseSection from "@/components/WhichStackToUseSection.vue";
 
 export default {
-  name: 'MainPage',
+  name: "MainPage",
   components: {
     MainBlock,
     SpecialistsInfo,
@@ -32,7 +32,7 @@ export default {
     LifeOutsideOffice,
     WhichStackToUseSection,
     MapSection,
-    EducationSection
+    EducationSection,
   },
 };
 </script>
