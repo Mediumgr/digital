796320303397e6b22ee1db17365b137a8af96eae
Руслан <Mediumgr@yandex.ru>
2023-11-19 02:47:58 +0300

    check images loading

--- src/components/FormVacancy.vue
+++ src/components/FormVacancy.vue
@@ -198,11 +198,11 @@ const removeFile = (event) => {
 
 const mouseover = (event) => {
   if (event.target.classList.contains("vacancy__card")) {
-    // clearInterval(interval.value);
+    clearInterval(interval.value);
     interval.value = setInterval(() => {
       initialBackground.value = false;
       currentIndex.value = (currentIndex.value + 1) % imageURLs.value.length;
-    }, 300);
+    }, 1000);
   }
 };
 /*  */
