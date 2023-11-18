229bb10d7c2e80ab26d2bdb79037b84300907cdd
Руслан <Mediumgr@yandex.ru>
2023-11-19 02:12:24 +0300

    check images loading

--- src/components/FormVacancy.vue
+++ src/components/FormVacancy.vue
@@ -228,9 +228,8 @@ const backgroundStyle = computed(() => {
 // const loadedImages = ref([]);
 const preLoadImages = () => {
   /* загрузка всех изображений для вакансий в кэш браузера */
-  // const imagePromises =
-  imageURLs.value.forEach((url) => {
-    new Promise((resolve, reject) => {
+  const imagePromises = imageURLs.value.map((url) => {
+    return new Promise((resolve, reject) => {
       const img = new Image();
       img.onerror = reject;
       img.src = `${require(`@/assets/images/vacancies/${url}`)}`;
@@ -238,19 +237,19 @@ const preLoadImages = () => {
     });
   });
 
-  // Promise.all(imagePromises)
-  //   .then((images) => {
-  //     images.forEach((img) => {
-  //       const regex = /^.*\/img\//;
-  //       // debugger;
-  //       const result = img.src.replace(regex, "");
-  //       loadedImages.value.push(result);
-  //       console.log("img_vacancy", img);
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.error("Ошибка при загрузке изображений:", error);
-  //   });
+  Promise.all(imagePromises)
+    // .then((images) => {
+    //   images.forEach((img) => {
+    //     const regex = /^.*\/img\//;
+    //     // debugger;
+    //     const result = img.src.replace(regex, "");
+    //     loadedImages.value.push(result);
+    //     console.log("img_vacancy", img);
+    //   });
+    // })
+    // .catch((error) => {
+    //   console.error("Ошибка при загрузке изображений:", error);
+    // });
 
   /* загрузка изображения на случай отсутствия интернета у пользователя */
   new Promise((resolve, reject) => {
