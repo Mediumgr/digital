8e4f6ccd71a1a15b8f15d4e12ea1ae72f7d8a8e6
Slava <bitrix@maxima.ru>
2023-12-11 14:01:01 +0300

    Стопка карт

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -153,6 +153,7 @@
       <div class="wrapper">
         <h1 class="wrapper__title">Давай знакомиться</h1>
         <div class="wrapper__content">
+          <!--
           <button
             class="wrapper__about"
             @mouseover="changeVideoSource('about')"
@@ -160,6 +161,23 @@
           >
             О нас
           </button>
+          -->
+          <button
+            class="wrapper__vacancy"
+            @mouseover="changeVideoSource('about')"
+            @click.stop="scrollTo('vacancy')"
+          >
+            Стать частью команды
+          </button>
+
+          <button
+            class="wrapper__recommend"
+            @mouseover="changeVideoSource('life')"
+            @click.stop="scrollTo('recommend')"
+          >
+            Привести друга
+          </button>
+
           <button
             class="wrapper__projects"
             @mouseover="changeVideoSource('projects')"
@@ -179,8 +197,9 @@
             @mouseover="changeVideoSource('working')"
             @click.stop="scrollTo('work')"
           >
-            Работа с нами
+            Этапы найма
           </button>
+          <!--
           <button
             class="wrapper__life"
             @mouseover="changeVideoSource('life')"
@@ -188,6 +207,8 @@
           >
             Жизнь вне офиса
           </button>
+          -->
+          <!--
           <button
             class="wrapper__develop"
             @mouseover="changeVideoSource('develop')"
@@ -195,6 +216,7 @@
           >
             Развитие
           </button>
+          -->
         </div>
         <button
           class="wrapper__arrow"
@@ -274,6 +296,14 @@ const scrollTo = (value) => {
       break;
     case "aboutUs":
       element = document.querySelector(".specialists__number");
+      offsetElement = element.getBoundingClientRect().top;
+      break;
+    case "vacancy":
+      element = document.querySelector(".vacancy__section");
+      offsetElement = element.getBoundingClientRect().top;
+      break;
+    case "recommend":
+      element = document.querySelector(".recommend__container");
       offsetElement = element.getBoundingClientRect().top;
       break;
   }
@@ -381,13 +411,13 @@ const intersectionAnimation = () => {
             setTimeout(() => {
               title.style.opacity =
                 content.style.opacity =
-                arrow.style.opacity =
-                  "1";
+                  arrow.style.opacity =
+                    "1";
             }, 0);
             title.style.display =
               content.style.display =
-              arrow.style.display =
-                "";
+                arrow.style.display =
+                  "";
             gradientGroup.value.style.opacity = opacity[6];
           }
           if (videoTopPosition.value > 300) {
@@ -470,9 +500,9 @@ onMounted(async () => {
   height: 100%;
   border-radius: 4rem;
   background: linear-gradient(
-    180deg,
-    rgba(0, 0, 0, 0) 0%,
-    rgba(0, 0, 0, 0.6) 58.51%
+      180deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.6) 58.51%
   );
   display: flex;
   justify-content: center;
@@ -533,8 +563,8 @@ onMounted(async () => {
       height: 22rem;
       margin: 0 auto 19.5rem;
       grid-template-areas:
-        "about projects work"
-        "office life develop";
+"vacancy . recommend"
+"work projects office"
     }
     @include mq(810) {
       margin: 0px auto 5rem;
@@ -550,11 +580,11 @@ onMounted(async () => {
       width: auto;
       height: auto;
       margin-bottom: 7rem;
-      grid-template-columns: auto auto auto 24rem;
+      grid-template-columns: auto auto auto;
       grid-template-rows: 1fr 1fr;
       grid-template-areas:
-        "about projects work office"
-        ". life develop .";
+"vacancy . recommend"
+"work projects office"
     }
     > button {
       border-radius: 16rem;
@@ -602,6 +632,34 @@ onMounted(async () => {
     @include mq(1440) {
       height: 11.8rem;
       width: 20.5rem;
+    }
+  }
+
+  &__vacancy {
+    height: 5.2rem;
+    justify-self: center;
+
+    @include mq(768) {
+      height: 9rem;
+      grid-area: vacancy;
+      justify-self: start;
+    }
+    @include mq(1440) {
+      height: 11.8rem;
+    }
+  }
+
+  &__recommend {
+    height: 5.2rem;
+    justify-self: center;
+
+    @include mq(768) {
+      height: 9rem;
+      grid-area: recommend;
+      justify-self: start;
+    }
+    @include mq(1440) {
+      height: 11.8rem;
     }
   }
 
@@ -1192,23 +1250,23 @@ header {
 
 .gradient-group .purple {
   background: radial-gradient(
-    50% 50% at 50% 50%,
-    rgb(143, 20, 186) 0%,
-    rgba(143, 20, 186, 0.99) 11.79%,
-    rgba(143, 20, 186, 0.97) 21.38%,
-    rgba(143, 20, 186, 0.93) 29.12%,
-    rgba(143, 20, 186, 0.88) 35.34%,
-    rgba(143, 20, 186, 0.82) 40.37%,
-    rgba(143, 20, 186, 0.75) 44.56%,
-    rgba(143, 20, 186, 0.67) 48.24%,
-    rgba(143, 20, 186, 0.59) 51.76%,
-    rgba(143, 20, 186, 0.5) 55.44%,
-    rgba(143, 20, 186, 0.41) 59.63%,
-    rgba(143, 20, 186, 0.33) 64.66%,
-    rgba(143, 20, 186, 0.24) 70.88%,
-    rgba(143, 20, 186, 0.15) 78.62%,
-    rgba(143, 20, 186, 0.07) 88.21%,
-    rgba(143, 20, 186, 0) 100%
+      50% 50% at 50% 50%,
+      rgb(143, 20, 186) 0%,
+      rgba(143, 20, 186, 0.99) 11.79%,
+      rgba(143, 20, 186, 0.97) 21.38%,
+      rgba(143, 20, 186, 0.93) 29.12%,
+      rgba(143, 20, 186, 0.88) 35.34%,
+      rgba(143, 20, 186, 0.82) 40.37%,
+      rgba(143, 20, 186, 0.75) 44.56%,
+      rgba(143, 20, 186, 0.67) 48.24%,
+      rgba(143, 20, 186, 0.59) 51.76%,
+      rgba(143, 20, 186, 0.5) 55.44%,
+      rgba(143, 20, 186, 0.41) 59.63%,
+      rgba(143, 20, 186, 0.33) 64.66%,
+      rgba(143, 20, 186, 0.24) 70.88%,
+      rgba(143, 20, 186, 0.15) 78.62%,
+      rgba(143, 20, 186, 0.07) 88.21%,
+      rgba(143, 20, 186, 0) 100%
   );
   border-radius: 13rem;
   position: absolute;
@@ -1217,14 +1275,14 @@ header {
   top: -40rem;
   left: -40rem;
   animation: slideInGradient 1.5s ease forwards,
-    gradientPurple_375 8s ease infinite;
+  gradientPurple_375 8s ease infinite;
   @include mq(375) {
     height: 130rem;
     width: 130rem;
     top: -40rem;
     left: -40rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientPurple_375 8s ease infinite;
+    gradientPurple_375 8s ease infinite;
   }
 
   @include mq(768) {
@@ -1233,7 +1291,7 @@ header {
     top: -40rem;
     left: -35rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientPurple_768 8s ease infinite;
+    gradientPurple_768 8s ease infinite;
   }
 
   @include mq(1024) {
@@ -1242,7 +1300,7 @@ header {
     top: -70rem;
     left: -35rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientPurple_1024 8s ease infinite;
+    gradientPurple_1024 8s ease infinite;
   }
 
   @include mq(1440) {
@@ -1251,7 +1309,7 @@ header {
     top: -80rem;
     left: -50rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientPurple_1440 8s ease infinite;
+    gradientPurple_1440 8s ease infinite;
   }
 
   @include mq(1920) {
@@ -1260,36 +1318,36 @@ header {
     top: -80rem;
     left: -30rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientPurple_1920 8s ease infinite;
+    gradientPurple_1920 8s ease infinite;
   }
 
   @include mq(2560) {
     top: -110rem;
     left: -60rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientPurple_2560 8s ease infinite;
+    gradientPurple_2560 8s ease infinite;
   }
 }
 
 .gradient-group .red {
   background: radial-gradient(
-    50% 50% at 50% 50%,
-    rgb(255, 80, 48) 0%,
-    rgba(254.66, 79.88, 48.43, 0.99) 11.3%,
-    rgba(253.67, 79.55, 49.67, 0.97) 21.16%,
-    rgba(252.1, 79.01, 51.63, 0.93) 29.76%,
-    rgba(250.01, 78.3, 54.24, 0.89) 37.27%,
-    rgba(247.46, 77.43, 57.42, 0.83) 43.87%,
-    rgba(244.53, 76.43, 61.09, 0.76) 49.76%,
-    rgba(241.26, 75.32, 65.17, 0.69) 55.1%,
-    rgba(237.73, 74.11, 69.58, 0.61) 60.08%,
-    rgba(234.01, 72.84, 74.24, 0.52) 64.88%,
-    rgba(230.14, 71.53, 79.07, 0.44) 69.68%,
-    rgba(226.21, 70.18, 83.99, 0.35) 74.66%,
-    rgba(222.26, 68.84, 88.92, 0.26) 80%,
-    rgba(218.37, 67.51, 93.79, 0.17) 85.88%,
-    rgba(214.59, 66.22, 98.51, 0.08) 92.49%,
-    rgba(211, 65, 103, 0) 100%
+      50% 50% at 50% 50%,
+      rgb(255, 80, 48) 0%,
+      rgba(254.66, 79.88, 48.43, 0.99) 11.3%,
+      rgba(253.67, 79.55, 49.67, 0.97) 21.16%,
+      rgba(252.1, 79.01, 51.63, 0.93) 29.76%,
+      rgba(250.01, 78.3, 54.24, 0.89) 37.27%,
+      rgba(247.46, 77.43, 57.42, 0.83) 43.87%,
+      rgba(244.53, 76.43, 61.09, 0.76) 49.76%,
+      rgba(241.26, 75.32, 65.17, 0.69) 55.1%,
+      rgba(237.73, 74.11, 69.58, 0.61) 60.08%,
+      rgba(234.01, 72.84, 74.24, 0.52) 64.88%,
+      rgba(230.14, 71.53, 79.07, 0.44) 69.68%,
+      rgba(226.21, 70.18, 83.99, 0.35) 74.66%,
+      rgba(222.26, 68.84, 88.92, 0.26) 80%,
+      rgba(218.37, 67.51, 93.79, 0.17) 85.88%,
+      rgba(214.59, 66.22, 98.51, 0.08) 92.49%,
+      rgba(211, 65, 103, 0) 100%
   );
   border-radius: 13rem;
   position: absolute;
@@ -1298,7 +1356,7 @@ header {
   top: -25rem;
   left: -15rem;
   animation: slideInGradient 1.5s ease forwards,
-    gradientRed_375 8s ease infinite;
+  gradientRed_375 8s ease infinite;
 
   @include mq(375) {
     width: 85rem;
@@ -1306,7 +1364,7 @@ header {
     top: -25rem;
     left: -15rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientRed_375 8s ease infinite;
+    gradientRed_375 8s ease infinite;
   }
 
   @include mq(768) {
@@ -1315,7 +1373,7 @@ header {
     top: -30rem;
     left: 5rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientRed_768 8s ease infinite;
+    gradientRed_768 8s ease infinite;
   }
 
   @include mq(1024) {
@@ -1324,14 +1382,14 @@ header {
     top: -35rem;
     left: 0;
     animation: slideInGradient 1.5s ease forwards,
-      gradientRed_1024 8s ease infinite;
+    gradientRed_1024 8s ease infinite;
   }
 
   @include mq(1440) {
     top: -45rem;
     left: 35rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientRed_1440 8s ease infinite;
+    gradientRed_1440 8s ease infinite;
   }
 
   @include mq(1920) {
@@ -1340,36 +1398,36 @@ header {
     top: -60rem;
     left: 10rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientRed_1920 8s ease infinite;
+    gradientRed_1920 8s ease infinite;
   }
 
   @include mq(2560) {
     top: -70rem;
     left: 80rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientRed_2560 8s ease infinite;
+    gradientRed_2560 8s ease infinite;
   }
 }
 
 .gradient-group .yellow {
   background: radial-gradient(
-    50% 50% at 50% 50%,
-    rgb(255, 168, 72) 0%,
-    rgba(255, 167.23, 71.79, 0.99) 11.31%,
-    rgba(255, 165.19, 71.23, 0.97) 21.19%,
-    rgba(255, 162, 70.36, 0.93) 29.81%,
-    rgba(255, 157.8, 69.22, 0.88) 37.35%,
-    rgba(255, 152.71, 67.83, 0.83) 44%,
-    rgba(255, 146.84, 66.23, 0.76) 49.92%,
-    rgba(255, 140.33, 64.45, 0.69) 55.29%,
-    rgba(255, 133.3, 62.54, 0.61) 60.3%,
-    rgba(255, 125.88, 60.51, 0.52) 65.12%,
-    rgba(255, 118.18, 58.41, 0.43) 69.92%,
-    rgba(255, 110.34, 56.27, 0.34) 74.89%,
-    rgba(255, 102.47, 54.13, 0.26) 80.21%,
-    rgba(255, 94.71, 52.01, 0.17) 86.05%,
-    rgba(255, 87.18, 49.96, 0.08) 92.59%,
-    rgba(255, 80, 48, 0) 100%
+      50% 50% at 50% 50%,
+      rgb(255, 168, 72) 0%,
+      rgba(255, 167.23, 71.79, 0.99) 11.31%,
+      rgba(255, 165.19, 71.23, 0.97) 21.19%,
+      rgba(255, 162, 70.36, 0.93) 29.81%,
+      rgba(255, 157.8, 69.22, 0.88) 37.35%,
+      rgba(255, 152.71, 67.83, 0.83) 44%,
+      rgba(255, 146.84, 66.23, 0.76) 49.92%,
+      rgba(255, 140.33, 64.45, 0.69) 55.29%,
+      rgba(255, 133.3, 62.54, 0.61) 60.3%,
+      rgba(255, 125.88, 60.51, 0.52) 65.12%,
+      rgba(255, 118.18, 58.41, 0.43) 69.92%,
+      rgba(255, 110.34, 56.27, 0.34) 74.89%,
+      rgba(255, 102.47, 54.13, 0.26) 80.21%,
+      rgba(255, 94.71, 52.01, 0.17) 86.05%,
+      rgba(255, 87.18, 49.96, 0.08) 92.59%,
+      rgba(255, 80, 48, 0) 100%
   );
   border-radius: 13rem;
   position: absolute;
@@ -1378,7 +1436,7 @@ header {
   top: 40rem;
   left: 18rem;
   animation: slideInGradient 1.5s ease forwards,
-    gradientYellow_375 8s ease infinite;
+  gradientYellow_375 8s ease infinite;
 
   @include mq(375) {
     width: 40rem;
@@ -1386,7 +1444,7 @@ header {
     top: 40rem;
     left: 18rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientYellow_375 8s ease infinite;
+    gradientYellow_375 8s ease infinite;
   }
 
   @include mq(768) {
@@ -1395,7 +1453,7 @@ header {
     top: 45rem;
     left: 35rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientYellow_768 8s ease infinite;
+    gradientYellow_768 8s ease infinite;
   }
 
   @include mq(1024) {
@@ -1404,7 +1462,7 @@ header {
     top: 35rem;
     left: 55rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientYellow_1024 8s ease infinite;
+    gradientYellow_1024 8s ease infinite;
   }
 
   @include mq(1440) {
@@ -1413,7 +1471,7 @@ header {
     top: 20rem;
     left: 95rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientYellow_1440 8s ease infinite;
+    gradientYellow_1440 8s ease infinite;
   }
 
   @include mq(1920) {
@@ -1422,14 +1480,14 @@ header {
     top: 30rem;
     left: 100rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientYellow_1920 8s ease infinite;
+    gradientYellow_1920 8s ease infinite;
   }
 
   @include mq(2560) {
     top: 30rem;
     left: 170rem;
     animation: slideInGradient 1.5s ease forwards,
-      gradientYellow_2560 8s ease infinite;
+    gradientYellow_2560 8s ease infinite;
   }
 }
 
