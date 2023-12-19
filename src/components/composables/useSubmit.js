a0c440610125961cca949f287b504beb562f6152
Руслан <Mediumgr@yandex.ru>
2023-12-19 13:21:46 +0300

    обновлен функционал формы

--- src/components/composables/useSubmit.js
+++ src/components/composables/useSubmit.js
@@ -1,4 +1,4 @@
-import { useValidate } from './useValidate';
+import { useValidate } from "./useValidate";
 
 export const useSubmit = async (options) => {
   const { name, email, tel, file, regExp, errors, loader, status } = options;
@@ -14,7 +14,7 @@ export const useSubmit = async (options) => {
 
   let isError = false;
   for (let err in errors.value) {
-    if (errors.value[err] === 'error') {
+    if (errors.value[err] === "error") {
       isError = true;
       break;
     }
@@ -22,17 +22,25 @@ export const useSubmit = async (options) => {
 
   if (isError === false) {
     status.connection = true;
-
     loader.value = true;
+
+    let { utmMedium, utmSource, utmCampaign, clickId } = extractUTMParams(
+      window.location.search
+    );
+
     let formData = new FormData();
 
-    formData.append('name', name.value.text);
-    formData.append('email', email.value.text);
-    formData.append('phone', tel.value.text);
-    formData.append('file', file.value);
+    formData.append("name", name.value.text);
+    formData.append("email", email.value.text);
+    formData.append("phone", tel.value.text);
+    formData.append("file", file.value);
+    formData.append("utm[utm_source]", utmSource);
+    formData.append("utm[utm_medium]", utmMedium);
+    formData.append("utm[utm_campaign]", utmCampaign);
+    formData.append("click_id", clickId);
 
-    let res = await fetch('/upload/', {
-      method: 'POST',
+    let res = await fetch("/upload/", {
+      method: "POST",
       body: formData,
     });
 
@@ -41,12 +49,37 @@ export const useSubmit = async (options) => {
     if (response.error === true) {
       status.server.failure = true;
       status.server.message = response.message;
-      delete status['connection'];
+      delete status["connection"];
+    } else {
+      console.log("yaReachGoal invoked");
+      yaReachGoal("target");
     }
 
     loader.value = false;
     status.form = false;
   }
+};
+
+const yaReachGoal = (idTarget) => {
+  if (typeof ym == "function") {
+    try {
+      /* eslint-disable */
+      ym(95461603, "reachGoal", idTarget);
+      console.log("ReachGoal push");
+    } catch (e) {
+      console.log("ReachGoal error", e);
+    }
+  }
+};
+
+const extractUTMParams = (params) => {
+  const urlParams = new URLSearchParams(params);
+  const utmMedium = urlParams.get("utm_medium") || "";
+  const utmSource = urlParams.get("utm_source") || "";
+  const utmCampaign = urlParams.get("utm_campaign") || "";
+  const clickId = urlParams.get("click_id") || "";
+
+  return { utmMedium, utmSource, utmCampaign, clickId };
 };
 
 const checkInternetConnection = () => {
